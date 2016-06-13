164f159a0b610844a58b81bd2787c91b7fd19eb3
huniSkip <houshengp@163.com>
2016-06-13 11:07:32 +0800

    在Appdeledate里面修改

---
+++ 聊天/聊天/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  聊天
+//
+//  Created by lanou3g on 16/6/13.
+//  Copyright © 2016年 huni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 聊天/聊天/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  聊天
+//
+//  Created by lanou3g on 16/6/13.
+//  Copyright © 2016年 huni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
